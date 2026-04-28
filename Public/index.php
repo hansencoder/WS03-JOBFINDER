54b385a368b175c2292a82cb5782ee5d3f4567bb
Hans Flores <floressktt11@gmail.com>
2026-04-29 00:39:46 +0800

    continucontinuation

---
+++ Public/index.php
@@ -0,0 +1,26 @@
+<?php
+
+require_once __DIR__ . '/../helpers.php';
+
+$routes = [
+    '/' => 'Controllers/home.php',
+    '/listings' => 'Controllers/listings/index.php',
+    '/listings/create' => 'Controllers/listings/create.php',
+    '404' => 'Controllers/error/404.php'
+];
+
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+// remove /WS03/Public from the URI
+$basePath = '/WS03/Public';
+if (str_starts_with($uri, $basePath)) {
+    $uri = substr($uri, strlen($basePath));
+}
+
+$uri = $uri ?: '/';
+
+if (array_key_exists($uri, $routes)) {
+    require basePath($routes[$uri]);
+} else {
+    require basePath($routes['404']);
+}
